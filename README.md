9639dad535c79540bb2de15e4366e32a8f1ea9d5
ColinCPittman <colincpittman@gmail.com>
2025-09-29 18:35:24 -0400

    Tweaked method names and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DECAF20-Parser
 This repository contains a simple scanner and parser for the [DECAF language](https://anoopsarkar.github.io/compilers-class/decafspec.html), written in Python.
+
+## Running the Scanner
+
+``` bash
+python main.py [.decaf file]
+```
+Output is a printed list of generated lexeme tokens based on the input .decaf file.
--- main.py
+++ main.py
@@ -8,19 +8,23 @@ class Token:
         self.end_col = end_col
         self.type = type
         self.is_operator = is_operator
-        if is_constant is not False:
-            self.is_constant = type == 'T_CHARCONSTANT' or type == 'T_INTCONSTANT' or type == 'T_STRINGCONSTANT'
+        self.is_constant = is_constant
+        if self.is_constant:
+            self.is_constant = type == 'T_CHARCONSTANT' or type == 'T_IntConstant' or type == 'T_STRINGCONSTANT' or type == 'T_BoolConstant'
         self.const_value = const_value
 
     def print_token(self):
-        if not self.is_operator:
-            print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is {self.type}")
+        if self.is_operator:
+            # 2-character operators like <=, &&, show the token type instead of the indentifier after 'is' in the expected output
+            if len(self.identifier) > 1:
+                print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is {self.type}")
+            else:
+                print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is '{self.identifier}'")
         elif self.is_constant: 
-            # the expected output shows that constant types repeat the value after the type (e.g. T_INTCONSTANT (value = 1))
-            print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is {self.type} (value = {self.identifier})")
+            # the expected output shows that constant types repeat the value after the type (e.g. T_IntConstant (value= 1))
+            print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is {self.type} (value= {self.identifier})")
         else: 
-            # the expected output shows that operators have their identifier printed in the stead of their type
-            print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is '{self.identifier}'")
+            print(f"{self.identifier} \t line {self.line} Cols {self.start_col} - {self.end_col} is {self.type}")
 
 class Scanner:
     def __init__(self, input):
@@ -48,38 +52,40 @@ class Scanner:
             '>>': 'T_RIGHTSHIFT',
             '<': 'T_LT',
             '>': 'T_GT',
-            '<=': 'T_LEQ',
+            '<=': 'T_LessEqual', #instead of 'T_LEQ', as per spec, to match expected output
             '>=': 'T_GEQ',
             '==': 'T_EQ',
             '!=': 'T_NEQ',
-            '&&': 'T_AND',
+            '&&': 'T_logicaland', # should be 'T_AND' as per the DECAF20 spec but changed to match expected output
             '||': 'T_OR',
             '.': 'T_DOT'
         }
+        #some of these keywords had to deviate from the all-caps format specified to match the expected output
         self.keywords = {
-            'bool': 'T_BOOLTYPE',
+            'bool': 'T_IDENTIFIER', # spec says this should be 'T_BOOLTYPE' but expected output shows 'T_IDENTIFIER'
             'break': 'T_BREAK',
             'continue': 'T_CONTINUE',
-            'else': 'T_ELSE',
+            'else': 'T_Else',
             'extern': 'T_EXTERN',
-            'false': 'T_FALSE',
-            'for': 'T_FOR',
+            'false': 'T_BoolConstant',
+            'for': 'T_For',
             'func': 'T_FUNC',
-            'if': 'T_IF',
-            'int': 'T_INTTYPE',
+            'if': 'T_If',
+            'int': 'T_Int',
             'null': 'T_NULL',
             'package': 'T_PACKAGE',
-            'return': 'T_RETURN',
-            'string': 'T_STRINGTYPE',
-            'true': 'T_TRUE',
+            'return': 'T_Return',
+            'string': 'T_String', # this is 'T_STRINGTYPE' in the spec but had to change it to 'T_String' to match expected output
+            'true': 'T_BoolConstant',
             'var': 'T_VAR',
-            'void': 'T_VOID',
-            'while': 'T_WHILE'
+            'void': 'T_Void',
+            'while': 'T_WHILE',
+            'Print': 'T_Print'
         }
         
     
     # letter => "A" ... "Z" | "a" ... "z" | "_"
-    def _char_isletter(self):
+    def _is_letter(self):
         return self.input[self.index].isalpha() or self.input[self.index] == "_"
 
     # hex_lit     => "0" ( "x" | "X" ) { hex_digit }
@@ -91,8 +97,8 @@ class Scanner:
     # escaped_char => "\" ( "n" | "r" | "t" | "v" | "f" | "a" | "b" | `\` | "'" | `"` )
     def _is_escaped_char(self):
         return (self.index + 1 < len(self.input) and 
-                self.input[self.index] == '\' and 
-                self.input[self.index + 1] in ['n', 'r', 't', 'v', 'f', 'a', 'b', '\', "'", '"'])
+                self.input[self.index] == '\\' and 
+                self.input[self.index + 1] in ['n', 'r', 't', 'v', 'f', 'a', 'b', '\\', "'", '"'])
     
     # char => all ASCII characters from 7 ... 13 and 32 ... 126 except char 10 "\n", char 92 "\" and char 34: "
     def _is_char(self):
@@ -117,20 +123,20 @@ class Scanner:
             if (self.input[self.index] == "'" 
                     or self.input[self.index] == '"'
                     or self.input[self.index].isdigit()):
-                self._scan_literals()
-            elif self._char_isletter():
+                self._scan_literal()
+            elif self._is_letter():
                 self._scan_alphanum()
-            elif self.input[self.index] in self.operators:
-                self._scan_operators()
-            elif self.input[self.index] == ' ':
-                self.col += 1
-                self.index += 1
-            elif self.input[self.index] == '\n':
-                self.line += 1
-                self.col = 1
+            elif self.input[self.index] in self.operators or self.input[self.index] == '&' or self.input[self.index] == '|':
+                self._scan_operator()
+            elif self.input[self.index].isspace():
+                if self.input[self.index] == '\n':
+                    self.line += 1
+                    self.col = 1
+                else:
+                    self.col += 1
                 self.index += 1
             else:
-                print(f"Error: Unexpected character: {self.input[self.index]} at row {self.line}, column {self.col}")
+                print(f"Error: Unexpected character: '{self.input[self.index]}' at line {self.line}, column {self.col}")
                 return
 
      
@@ -149,7 +155,7 @@ class Scanner:
     # _______________________________________________________
     # string_lit   => `"` { char | escaped_char } `"` .
     #
-    def _scan_literals(self):
+    def _scan_literal(self):
         initial_col = self.col
         identifier = ''
 
@@ -158,27 +164,34 @@ class Scanner:
             identifier += self._advance() + self._advance()  # consume ('0' and ('x' or 'X'))
             while self.index < len(self.input) and self.input[self.index].isxdigit():
                 identifier += self._advance()
-             self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'T_INTCONSTANT', is_const = True))
+            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'T_IntConstant', is_constant = True))
 
         #decimal literals
         elif self.input[self.index].isdigit():
-             while self.index < len(self.input) and self.input[self.index].isdigit():
+            while self.index < len(self.input) and self.input[self.index].isdigit():
                 identifier += self._advance()
-            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'T_INTCONSTANT', is_const = True))
+            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'T_IntConstant', is_constant = True))
         
         #string literals
         elif self.input[self.index] == '"':
             identifier += self._advance()
             while self.index < len(self.input) and self.input[self.index] != '"':
-                if self._is_char() or self._is_escaped_char():
-                #TODO
-        else: # starts with a "'"
-            pass
-
-        while self.index < len(self.input):
-            identifier += self._advance()
+                if self._is_escaped_char():
+                    identifier += self._advance() + self._advance() #consume / and the subsequent escaped_char
+                elif self._is_char(): 
+                    identifier += self._advance() # consume normal char
+            identifier += self._advance() # consume the end double quote
+            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, "T_STRINGCONSTANT", is_constant = True))
         
-        self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'placeholder'))
+        #character literals
+        else: # starts with a "'"
+            identifier +=  self._advance()
+            if self._is_escaped_char():
+                identifier += self._advance() + self._advance()
+            elif self._is_char_lit_chars():
+                identifier += self._advance()
+            identifier += self._advance() # consume ending single quote
+            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, "T_CHARCONSTANT", is_constant = True))
 
 
     # this function generates tokens for identifiers and keywords
@@ -186,15 +199,38 @@ class Scanner:
     def _scan_alphanum(self):
         initial_col = self.col
         identifier = ''
-        while self.index < len(self.input) and (self._char_isletter() or self.input[self.index].isdigit()):
+        while self.index < len(self.input) and (self._is_letter() or self.input[self.index].isdigit()):
             identifier += self._advance()
-        if identifier in self.keywords:
-            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, self.keywords[identifier]))
-        else:
-            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'T_ID'))
 
-    def _scan_operators(self):
-        pass
+        # keywords    
+        if identifier in self.keywords: 
+            if identifier == 'true' or identifier == 'false': #booleans are the only constant type the can be encountered in a keyword match
+                self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, self.keywords[identifier], is_constant = True))
+            else:
+                self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, self.keywords[identifier]))
+        
+        # identifiers
+        else:
+            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, 'T_IDENTIFIER')) #DECAF20 spec specifies this type label as 'T_ID' but changing it to 'T_IDENTIFIER' to match expected output
+
+    def _scan_operator(self):
+        initial_col = self.col
+        identifier = ''
+
+        # 2-character operators
+        if self.index + 1 < len(self.input) and ((self.input[self.index] + self.input[self.index + 1]) in self.operators):
+            identifier += self._advance() + self._advance()
+
+        # single character operators
+        else:
+            identifier += self._advance()
+        
+        if identifier in self.operators:
+            self.tokens.append(Token(identifier, self.line, initial_col, self.col - 1, self.operators[identifier], is_operator = True))
+        # catch the case where a | or an & appear alone, since the dispaching tokenize() method calls this method if a single one is encountered
+        else:
+            print(f"Error: Unexpected character: '{identifier}' at line {self.line}, column {initial_col}")
+            return
     
     def print_tokens(self):
         for token in self.tokens:
@@ -215,3 +251,6 @@ def main():
             scanner.print_tokens()
     except FileNotFoundError:
         print(f"{input_file} not found")
+
+if __name__ == "__main__":
+    main()
